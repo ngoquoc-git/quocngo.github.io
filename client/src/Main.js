cc5301c718a77969e73fd14922f33cc37a22eeac
Quoc Truong Ngo <72320787+ngoquoc-git@users.noreply.github.com>
2022-03-18 07:45:20 -0700

    create Main page

---
+++ client/src/Main.js
@@ -0,0 +1,9 @@
+import './Main.css';
+function Main(){
+    return(
+        <div ClassName="body">
+            This is a body part
+        </div>
+    );
+}
+export default Main;
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Main from './Main';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
