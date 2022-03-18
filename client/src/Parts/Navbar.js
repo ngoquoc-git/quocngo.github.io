1b914b85fa29c0360e558137e971fc6464a4b9de
quocngo-git <ngoquoc.work@gmail.com>
2022-03-18 15:28:12 -0700

    style NavBar

--- client/src/Parts/Navbar.js
+++ client/src/Parts/Navbar.js
@@ -1,25 +1,46 @@
 import '../Assets/Navbar.css';
+import 'react-ionicons';
+import { LogoClosedCaptioning, LogoGithub, LogoInstagram, LogoLinkedin, LogOutOutline, ReorderThreeOutline } from 'react-ionicons';
+import logo from '../Assets/Quoc-navy-logo.PNG';
 
 const Navbar = () => {
     return(
         <div className="Nav-body">
             <div className="flex-child" id="flex-1">
-                Name and logo
+                <div className="flex-child-1" id="flex-11">
+                    <img src={logo} alt="logo" className="logo"></img>
+                    <div className="flex-child-11">
+                        QUOC TRUONG NGO
+                    </div>
+                </div>  
             </div>
             <div className="flex-child" id="flex-2">
-                Space
+                <div className="flex-child-21">
+                    Profile
+                </div>
+                <div className="flex-child-21">
+                    Resume
+                </div>
+                <div className="flex-child-21">
+                    Portfolio
+                </div>
+                <div className="flex-child-21">
+                    Feedback
+                </div>
             </div>
             <div className="flex-child" id="flex-3">
-                linkedin
-            </div>
-            <div className="flex-child" id="flex-4">
-                github
-            </div>
-            <div className="flex-child" id="flex-5">
-                leetcode
-            </div>
-            <div className="flex-child" id="flex-6">
-                Menu bar
+                <div className="flex-child-2" id="flex-21">
+                    <LogoLinkedin/>
+                </div>
+                <div className="flex-child-2" id="flex-22">
+                    <LogoGithub/>
+                </div>
+                <div className="flex-child-2" id="flex-23">
+                    <LogoInstagram/>
+                </div>
+                <div className="flex-child-2" id="flex-24">
+                    <ReorderThreeOutline/>
+                </div>
             </div>
         </div>
     );
